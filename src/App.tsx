c9fd4adad87de3e91d58eb5b415029d013681bdd
Lucas Del Puerto <lucasgarciadragon@hotmail.com>
2022-03-24 22:31:28 -0300

    ✨ 🎨 Adding more style for the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import { useRef, useState } from 'react'
+import { ChangeEventHandler, useRef, useState } from 'react'
 import FormData from 'form-data';
 import CanvasDraw from 'react-canvas-draw';
 import resizeImage from './utils/resizeImage';  
 import api from './api';
 
+import { FiGrid, FiSend, FiTrash } from 'react-icons/fi';
+import { IoArrowUndo, IoBrushOutline, IoBrushSharp} from 'react-icons/io5'
+
 import './App.css'
-import { FiCornerDownLeft, FiGrid, FiTrash } from 'react-icons/fi';
-import { IoArrowUndo} from 'react-icons/io5'
 
 function App() {
   const canvasRef = useRef({} as CanvasDraw);
   const [showGrid, setShowGrid] = useState(true);
+  const [brushRadius, setBrushRadius] = useState(12);
+  const [brushColor, setBrushColor] = useState('#000'); 
 
   async function submit() {
     //@ts-ignore
@@ -46,6 +49,25 @@ function App() {
     }
   }
 
+  function clear() {
+    canvasRef.current.clear();
+  }
+
+  function undo() {
+    canvasRef.current.undo();
+  }
+
+  function handleUpdateBrushSize(e: any) {
+    const value = Number(e.target.value);
+
+    if(value > 20)  
+      setBrushRadius(20);
+    else if(value < 1) 
+      setBrushRadius(1);
+    else
+      setBrushRadius(value);
+  }
+  
   return (
     <>
       <main className="App">
@@ -56,26 +78,44 @@ function App() {
             canvasWidth={700} 
             canvasHeight={500}
             backgroundColor='#fff'
-            brushColor='#000'
-            brushRadius={12}
-            hideGrid={showGrid}
+            brushColor={brushColor}
+            brushRadius={brushRadius}
+            hideGrid={true}
           />
           <span className='floating-controls'>
-            <button className='floating-button' onClick={canvasRef.current.clear}>
+            <button className='floating-button' onClick={clear}>
               <FiTrash />
             </button>
 
-            <button className='floating-button' onClick={canvasRef.current.undo}>
+            <button className='floating-button' onClick={undo}>
               <IoArrowUndo />
             </button>
 
-            {/* <button className='floating-button' disabled>
+            <button className='floating-button' disabled>
               <FiGrid />
-            </button> */}
+            </button>
           </span>
         </div>
-        <div className='controls'>
-          <button onClick={submit}>submit</button>
+        <div className='floor-controls'>
+          <div className='brush-controls'>
+            <IoBrushSharp color='#000'/>
+            <input 
+              type='color'  
+              value={brushColor}
+              onChange={(e) => setBrushColor(e.target.value)}
+            />
+            <input 
+              type='number' 
+              min={0} 
+              max={20} 
+              value={brushRadius} 
+              onChange={handleUpdateBrushSize}
+            />
+          </div>
+
+          <button className='floating-button send' onClick={submit}>
+            <FiSend />
+          </button>
         </div>
       </main>
     </>
